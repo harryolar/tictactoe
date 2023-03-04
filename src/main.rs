097031e069659dd3105aaeab0a965de3ab3b3dc9
Harry Olar <Harry.Olar@AppliedVision.com>
2023-03-04 10:52:45 -0500

    added the project files

---
+++ src/main.rs
@@ -0,0 +1,601 @@
+use rand::Rng;
+use std::io;
+use std::process;
+
+const EMPTY: usize = 0;
+const HUMAN: usize = 1;
+const AI: usize = 2;
+
+enum GameStateMachine {
+    Starting,
+    HumanPlaying,
+    AIPlaying,
+    GameEnded,
+    ExitGame,
+}
+
+fn get_user_move() -> usize {
+    let mut guess = String::new();
+    let mut is_value_ok = false;
+    while is_value_ok == false {
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        if guess.trim() == "Q" || guess.trim() == "q" {
+            println!("Quiting");
+            process::exit(0x00);
+        }
+
+        let x: usize = guess.trim().parse::<usize>().expect("Parsable");
+        if x > 9 {
+            println!("The number is outside the range.Try again")
+        } else {
+            return x;
+        }
+    }
+    return 0;
+}
+
+fn get_human_move(m: &mut [usize], move_count: usize) {
+    let number: usize = get_user_move();
+    if m[number] == EMPTY {
+        m[number] = HUMAN;
+
+        let human_moves: usize = check_player(m, HUMAN);
+        if human_moves == 3 {
+            println!("You won! Congrats ! Quiting the game ");
+            process::exit(0x00);
+        }
+    } else {
+        println!("The place is occupied")
+    }
+}
+
+fn get_rand_not_occupied_move(board: &mut [usize]) -> Option<usize> {
+    let mut move_count: usize = board.len();
+
+    if move_count == 0 {
+        return None; //board full
+    }
+
+    for i in board.iter_mut() {
+        if *i != EMPTY {
+            move_count = move_count - 1;
+        }
+    }
+
+    if move_count == 0 {
+        return None; //board full
+    }
+
+    let mut num: usize = 1;
+
+    if move_count > 1 {
+        num = rand::thread_rng().gen_range(1..move_count);
+    }
+
+    for (pos, i) in board.iter_mut().enumerate() {
+        if *i == EMPTY {
+            if num == 1 {
+                return Some(pos);
+            }
+            num = num - 1;
+        }
+    }
+
+    return None; //bad
+}
+
+fn get_ai_move(board: &mut [usize], move_count: usize) {
+    if move_count == 0 {
+        println!("My first shiny ass move:");
+        // Generate random number in the range [0, 8]
+        let num: usize = rand::thread_rng().gen_range(0..8);
+        println!("{}", num);
+        board[num] = AI;
+    } else {
+        let human_moves: usize = check_player(board, HUMAN);
+        if human_moves == 3 {
+            println!("You won ! Quiting the game");
+            process::exit(0x00);
+        } else if human_moves == 2 {
+            set_ai_next_move_when_2_occupied(board, HUMAN);
+        }
+
+        let ai_moves: usize = check_player(board, AI);
+        if ai_moves == 3 {
+            println!("I won ! Quiting the game");
+            process::exit(0x00);
+        } else if ai_moves == 2 {
+            set_ai_next_move_when_2_occupied(board, AI);
+        } else {
+            let m1 = get_rand_not_occupied_move(board);
+            match m1 {
+                Some(x) => board[x] = AI,
+                None => {
+                    println!("Error: There is no move");
+                    process::exit(0x00);
+                }
+            }
+        }
+    }
+}
+
+fn print_board(board: &[usize]) {
+    println!("{} {} {}", board[0], board[1], board[2]);
+    println!("{} {} {}", board[3], board[4], board[5]);
+    println!("{} {} {}", board[6], board[7], board[8]);
+}
+
+fn set_ai_next_move_when_2_occupied(board: &mut [usize], player: usize) {
+    
+    if board[0] == player && board[1] == player && board[2] == EMPTY {
+        board[2] = AI;
+    }
+    else if board[0] == EMPTY && board[1] == player && board[2] == player {
+        board[0] = AI;
+    }
+    else if board[0] == player && board[1] == EMPTY && board[2] == player {
+        board[1] = AI;
+    }
+    else if board[3] == player && board[4] == player && board[5] == EMPTY {
+        board[5] = AI;
+    }
+    else if board[3] == EMPTY && board[4] == player && board[5] == player {
+        board[3] = AI;
+    }
+    else if board[3] == player && board[4] == EMPTY && board[5] == player {
+        board[4] = AI;
+    }
+    else if board[6] == player && board[7] == player && board[8] == EMPTY {
+        board[8] = AI;
+    }
+    else if board[6] == EMPTY && board[7] == player && board[8] == player {
+        board[6] = AI;
+    }
+    else if board[6] == player && board[7] == EMPTY && board[8] == player {
+        board[7] = AI;
+    }
+    else if board[0] == player && board[3] == player && board[6] == EMPTY {
+        board[6] = AI;
+    }
+    else if board[0] == EMPTY && board[3] == player && board[6] == player {
+        board[0] = AI;
+    }
+    else if board[0] == player && board[3] == EMPTY && board[6] == player {
+        board[3] = AI;
+    }
+    else if board[1] == player && board[4] == player && board[7] == EMPTY {
+        board[7] = AI;
+    }
+    else if board[1] == EMPTY && board[4] == player && board[7] == player {
+        board[1] = AI;
+    }
+    else if board[1] == player && board[4] == EMPTY && board[7] == player {
+        board[4] = AI;
+    }
+    else if board[2] == player && board[5] == player && board[8] == EMPTY {
+        board[8] = AI;
+    }
+    else if board[2] == EMPTY && board[5] == player && board[8] == player {
+        board[2] = AI;
+    }
+    else if board[2] == player && board[5] == EMPTY && board[8] == player {
+        board[5] = AI;
+    }
+    else if board[0] == player && board[4] == player && board[8] == EMPTY {
+        board[8] = AI;
+    }
+    else if board[0] == player && board[4] == EMPTY && board[8] == player {
+        board[4] = AI;
+    }
+    else if board[0] == EMPTY && board[4] == player && board[8] == player {
+        board[0] = AI;
+    }
+    else if board[2] == player && board[4] == player && board[6] == EMPTY {
+        board[6] = AI;
+    }
+    else if board[2] == player && board[4] == EMPTY && board[6] == player {
+        board[4] = AI;
+    }
+    else if board[2] == EMPTY && board[4] == player && board[6] == player {
+        board[2] = AI;
+    }
+    else {
+        panic!("Couldn't find any moves");
+    }
+
+}
+
+fn check_player(board: &[usize], player: usize) -> usize {
+   
+    if board[0] == player && board[1] == player && board[2] == player
+        || board[3] == player && board[4] == player && board[5] == player
+        || board[6] == player && board[7] == player && board[8] == player
+        || board[0] == player && board[3] == player && board[6] == player
+        || board[1] == player && board[4] == player && board[7] == player
+        || board[2] == player && board[5] == player && board[8] == player
+        || board[0] == player && board[4] == player && board[8] == player
+        || board[2] == player && board[4] == player && board[6] == player
+    {
+        return 3;
+    }
+    else if board[0] == player && board[1] == player
+        || board[1] == player && board[2] == player
+        || board[3] == player && board[4] == player
+        || board[4] == player && board[5] == player
+        || board[6] == player && board[7] == player
+        || board[7] == player && board[8] == player
+        || board[0] == player && board[3] == player
+        || board[3] == player && board[6] == player
+        || board[1] == player && board[4] == player
+        || board[4] == player && board[7] == player
+        || board[2] == player && board[5] == player
+        || board[5] == player && board[8] == player
+    {
+        return 2;
+    } else if board[0] == player
+        || board[1] == player
+        || board[2] == player
+        || board[3] == player
+        || board[4] == player
+        || board[5] == player
+        || board[6] == player
+        || board[7] == player
+        || board[8] == player
+    {
+        return 1;
+    }
+
+    return 0;
+}
+
+fn main() {
+    
+    let mut guess = String::new();
+    let mut m = [0; 9]; // tic tac toe board
+    let mut count: usize = 0;
+    let mut is_still_playing: bool = true;
+
+    let mut game_state_machine: GameStateMachine = GameStateMachine::Starting;
+
+    while is_still_playing {
+        match game_state_machine {
+            GameStateMachine::Starting => {
+                println!("Hello, to tic tac toe . Do you want to start? Y/N or Q for quit");
+                io::stdin()
+                    .read_line(&mut guess)
+                    .expect("Failed to read line");
+
+                if guess.trim() == "Q" || guess.trim() == "q" {
+                    game_state_machine = GameStateMachine::ExitGame;
+                } else if guess.trim() == "N" || guess.trim() == "n" {
+                    game_state_machine = GameStateMachine::AIPlaying;
+                } else {
+                    game_state_machine = GameStateMachine::HumanPlaying;
+                    println!("Please input your first move between 0 and 8 :");
+                }
+            }
+
+            GameStateMachine::HumanPlaying => {
+                get_human_move(&mut m, count);
+                print_board(&mut m);
+                count = count + 1;
+                if count >= 9 {
+                    game_state_machine = GameStateMachine::GameEnded;
+                }
+                game_state_machine = GameStateMachine::AIPlaying;
+            }
+
+            GameStateMachine::AIPlaying => {
+                get_ai_move(&mut m, count);
+                print_board(&mut m);
+                count = count + 1;
+                if count >= 9 {
+                    game_state_machine = GameStateMachine::GameEnded;
+                }
+                game_state_machine = GameStateMachine::HumanPlaying;
+            }
+
+            GameStateMachine::GameEnded => {
+                println!("Game over!");
+                game_state_machine = GameStateMachine::Starting;
+            }
+
+            GameStateMachine::ExitGame => {
+                is_still_playing = false;
+                println!("Quiting");
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn internal() {
+        assert_eq!(1, 1);
+
+        // panic!("Error the output is greater than 8");
+    }
+
+    #[test]
+    fn get_rand_not_occupied_move_test() {
+        let mut m = [0; 9]; // tic tac toe board
+        let mut count: u32 = 0;
+
+        for n in 1..100 {
+            let m1 = get_rand_not_occupied_move(&mut m);
+            match m1 {
+                Some(x) => {
+                    if x > 8 {
+                        panic!("Error the output is greater than 8");
+                    };
+                }
+                None => {
+                    panic!("Error the output is empty");
+                }
+            }
+        }
+    }
+
+    #[test]
+    fn get_rand_not_occupied_move_test2() {
+        let mut m = [0; 9]; // tic tac toe board
+
+        m[0] = 1;
+        m[1] = 1;
+        m[2] = 1;
+        m[3] = 1;
+        m[4] = 1;
+        m[5] = 1;
+        m[6] = 1;
+        m[7] = 1;
+        m[8] = 0;
+
+        let mut count: u32 = 0;
+        let m1 = get_rand_not_occupied_move(&mut m);
+        match m1 {
+            Some(x) => {
+                assert_eq!(
+                    x, 8,
+                    "we are testing filling the 8th position when empty {} and {}",
+                    x, 8
+                );
+            }
+            None => {
+                panic!("Error the output is empty");
+            }
+        }
+    }
+
+    #[test]
+    fn get_rand_not_occupied_move_test3() {
+        let mut m = [0; 9]; // tic tac toe board
+
+        m[0] = 0;
+        m[1] = 1;
+        m[2] = 1;
+        m[3] = 1;
+        m[4] = 1;
+        m[5] = 1;
+        m[6] = 1;
+        m[7] = 1;
+        m[8] = 1;
+
+        let mut count: u32 = 0;
+        let m1 = get_rand_not_occupied_move(&mut m);
+        match m1 {
+            Some(x) => {
+                assert_eq!(
+                    x, 0,
+                    "we are testing filling the 0th position when empty {} and {}",
+                    x, 0
+                );
+            }
+            None => {
+                panic!("Error the output is empty");
+            }
+        }
+    }
+
+
+    #[test]
+    fn check_player_test() {
+        let mut m = [0; 9]; // tic tac toe board
+
+        m[0] = HUMAN;
+        m[1] = HUMAN;
+        m[2] = HUMAN;
+        m[3] = 0;
+        m[4] = 0;
+        m[5] = 0;
+        m[6] = 0;
+        m[7] = 0;
+        m[8] = 0;
+
+        let mut count: u32 = 0;
+
+        let human_moves: usize = check_player(&m, HUMAN);
+        
+        assert_eq!( human_moves, 3, "we are testing 3 human moves {} and {}",human_moves, 0);
+          
+    }
+
+    #[test]
+    fn check_player_test1() {
+        let mut m = [0; 9]; // tic tac toe board
+
+        m[0] = 0;
+        m[1] = 0;
+        m[2] = 0;
+        m[3] = HUMAN;
+        m[4] = HUMAN;
+        m[5] = HUMAN;
+        m[6] = 0;
+        m[7] = 0;
+        m[8] = 0;
+
+        let mut count: u32 = 0;
+
+        let human_moves: usize = check_player(&m, HUMAN);
+        
+        assert_eq!( human_moves, 3, "we are testing 3 human moves {} and {}",human_moves, 0);
+          
+    }
+
+    #[test]
+    fn check_player_test2() {
+        let mut m = [0; 9]; // tic tac toe board
+
+        m[0] = 0;
+        m[1] = 0;
+        m[2] = 0;
+        m[3] = 0;
+        m[4] = 0;
+        m[5] = 0;
+        m[6] = HUMAN;
+        m[7] = HUMAN;
+        m[8] = HUMAN;
+
+        let mut count: u32 = 0;
+
+        let human_moves: usize = check_player(&m, HUMAN);
+        
+        assert_eq!( human_moves, 3, "we are testing 3 human moves {} and {}",human_moves, 0);
+
+    }
+
+    #[test]
+    fn check_player_test3() {
+        let mut m = [0; 9]; // tic tac toe board
+
+        m[0] = HUMAN;
+        m[1] = 0;
+        m[2] = 0;
+        m[3] = 0;
+        m[4] = 0;
+        m[5] = 0;
+        m[6] = 0;
+        m[7] = 0;
+        m[8] = 0;
+
+        let mut count: u32 = 0;
+
+        let human_moves: usize = check_player(&m, HUMAN);
+        
+        assert_eq!( human_moves, 1, "we are testing 3 human moves {} and {}",human_moves, 1);
+          
+    }
+
+    #[test]
+    fn check_player_test4() {
+        let mut m = [0; 9]; // tic tac toe board
+
+        m[0] = 0;
+        m[1] = 0;
+        m[2] = 0;
+        m[3] = 0;
+        m[4] = 0;
+        m[5] = 0;
+        m[6] = 0;
+        m[7] = 0;
+        m[8] = HUMAN;
+
+        let mut count: u32 = 0;
+
+        let human_moves: usize = check_player(&m, HUMAN);
+        
+        assert_eq!( human_moves, 1, "we are testing 3 human moves {} and {}",human_moves, 1);
+          
+    }
+
+    #[test]
+    fn check_player_test5() {
+        let mut m = [0; 9]; // tic tac toe board
+
+        m[0] = 0;
+        m[1] = 0;
+        m[2] = 0;
+        m[3] = 0;
+        m[4] = 0;
+        m[5] = 0;
+        m[6] = HUMAN;
+        m[7] = 0;
+        m[8] = 0;
+
+        let mut count: u32 = 0;
+
+        let human_moves: usize = check_player(&mut m, HUMAN);
+        
+        assert_eq!( human_moves, 1, "we are testing 3 human moves {} and {}",human_moves, 0);
+          
+    }
+
+
+    #[test]
+    fn check_player_test6() {
+        let mut m = [0; 9]; // tic tac toe board
+
+        m[0] = 0;
+        m[1] = 0;
+        m[2] = 0;
+        m[3] = 0;
+        m[4] = 0;
+        m[5] = 0;
+        m[6] = 0;
+        m[7] = HUMAN;
+        m[8] = HUMAN;
+
+        let mut count: u32 = 0;
+
+        let human_moves: usize = check_player(&m, HUMAN);
+        
+        assert_eq!( human_moves, 2, "we are testing 3 human moves {} and {}",human_moves, 2);
+          
+    }
+
+    #[test]
+    fn set_ai_next_move_when_2_occupied_test() {
+        let mut m = [0; 9]; // tic tac toe board
+
+        m[0] = HUMAN;
+        m[1] = HUMAN;
+        m[2] = 0;
+        m[3] = 0;
+        m[4] = 0;
+        m[5] = 0;
+        m[6] = 0;
+        m[7] = 0;
+        m[8] = 0;
+        
+        set_ai_next_move_when_2_occupied(&mut m, HUMAN);
+        
+        assert_eq!( m[2], AI, "we are testing ai filling when 2 occupied {} and {}",m[2], AI);
+          
+    }
+
+    #[test] 
+    fn set_ai_next_move_when_2_occupied_test2() {
+        let mut m = [0; 9]; // tic tac toe board
+
+        m[0] = 0;
+        m[1] = HUMAN;
+        m[2] = HUMAN;
+        m[3] = 0;
+        m[4] = 0;
+        m[5] = 0;
+        m[6] = 0;
+        m[7] = 0;
+        m[8] = 0;
+        
+        set_ai_next_move_when_2_occupied(&mut m, HUMAN);
+        
+        assert_eq!( m[0], AI, "we are testing ai filling when 2 occupied {} and {}",m[0], AI);
+          
+    }
+}
+    
